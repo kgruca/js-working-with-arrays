c0c2103253814afe559b7783560e398ce823e348
Krzysztof Gruca <kgruca21@gmail.com>
2022-09-15 19:35:40 -0700

    array splice method

--- script.js
+++ script.js
@@ -75,6 +75,9 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+
+// NEW SECTION
+// Slice
 let arr = ['a', 'b', 'c', 'd', 'e'];
 // slice returns a new array and does't mutate the original array
 console.log(arr.slice(2));
@@ -90,3 +93,31 @@ console.log(arr.slice(1, -2));
 // can also use slice() to create a shallow copy of an array
 console.log(arr.slice());
 // logs ['a', 'b', 'c', 'd', 'e']
+
+
+// NEW SECTION
+// Splice
+// console.log(arr.splice(2));
+// logs ['c', 'd', 'e']
+// console.log(arr.splice(arr));
+// logs ['a', 'b']
+
+/* therefore, the big difference between slice() and splice() it:
+- slice() returns a new array and doesn't modify the original arr
+- splice() modifies the original arr
+*/
+
+// commenting out above to be able to continue practicing with methods
+
+// arr.splice(-1);
+// console.log(arr);
+// logs ['a', 'b', 'c', 'd']
+
+// when passing two args into the splice() method, the 2nd arg is deleteCount
+// so, the 2nd arg in splice() doesn't mean the ending
+// arr.splice(1, 2);
+// console.log(arr);
+// logs ['a', 'd']
+// so splicing starts at index 1 (b) and splices 2 elements away (b, and c)
+
+// commenting out above to be able to continue practicing with methods
